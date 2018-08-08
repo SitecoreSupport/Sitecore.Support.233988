891a16bc781d47c1bae0362c59ed7b84b20cc9c9
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2018-08-08 10:21:15 +0300

    Original code

---
+++ src/Sitecore.Support.233988/SolrIndex.cs
@@ -0,0 +1,48 @@
+﻿using Sitecore.ContentSearch.Linq.Common;
+using Sitecore.ContentSearch.Linq.Indexing;
+using Sitecore.ContentSearch.Linq.Parsing;
+using Sitecore.ContentSearch.Linq.Solr;
+using System;
+using System.Collections.Generic;
+
+namespace Sitecore.ContentSearch.Linq.Solr
+{
+  public class SolrIndex<TItem> : Index<TItem, SolrCompositeQuery>
+  {
+    private readonly SolrQueryOptimizer queryOptimizer = new SolrQueryOptimizer();
+
+    private readonly QueryMapper<SolrCompositeQuery> queryMapper;
+
+    private readonly SolrIndexParameters parameters;
+
+    protected override QueryMapper<SolrCompositeQuery> QueryMapper => queryMapper;
+
+    protected override IQueryOptimizer QueryOptimizer => queryOptimizer;
+
+    protected override FieldNameTranslator FieldNameTranslator => parameters.FieldNameTranslator;
+
+    protected override IIndexValueFormatter ValueFormatter => parameters.ValueFormatter;
+
+    public SolrIndexParameters Parameters => parameters;
+
+    public SolrIndex(SolrIndexParameters parameters)
+    {
+      if (parameters == null)
+      {
+        throw new ArgumentNullException("parameters");
+      }
+      queryMapper = new SolrQueryMapper(parameters);
+      this.parameters = parameters;
+    }
+
+    public override TResult Execute<TResult>(SolrCompositeQuery compositeQuery)
+    {
+      return default(TResult);
+    }
+
+    public override IEnumerable<TElement> FindElements<TElement>(SolrCompositeQuery compositeQuery)
+    {
+      return new List<TElement>();
+    }
+  }
+}
---
+++ src/Sitecore.Support.233988/SolrQuerySerializerUtility.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using SolrNet.Impl;
+using SolrNet.Impl.FieldSerializers;
+using SolrNet.Impl.QuerySerializers;
+using System;
+
+namespace Sitecore.ContentSearch.Linq.Solr
+{
+  internal static class SolrQuerySerializerUtility
+  {
+    public static string Serialize(object query)
+    {
+      return GetQuerySerializer().Serialize(query);
+    }
+
+    public static ISolrQuerySerializer GetQuerySerializer()
+    {
+      ISolrQuerySerializer solrQuerySerializer = null;
+      try
+      {
+        IServiceLocator current = ServiceLocator.Current;
+        solrQuerySerializer = ServiceLocator.Current.GetInstance<ISolrQuerySerializer>();
+      }
+      catch (NullReferenceException)
+      {
+      }
+      if (solrQuerySerializer == null)
+      {
+        solrQuerySerializer = new DefaultQuerySerializer(new DefaultFieldSerializer());
+      }
+      return solrQuerySerializer;
+    }
+  }
+}
